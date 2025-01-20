ce1cdda85f0a50bc1d914a1378f672815d12032a
Aphidet Jamphakhun <61898041+Aphidet6@users.noreply.github.com>
2025-01-20 22:51:19 +0700

    Add files via upload

--- app.js
+++ app.js
@@ -1,164 +1,157 @@
 const express = require('express');
 const path = require('path');
-const session = require('express-session'); // ติดตั้ง express-session
+// สร้าง Express app
 const app = express();
 
-// ตั้งค่า view engine ให้เป็น EJS
+// กำหนดพอร์ต
+const port = process.env.PORT || 3000;
+let title = 'Earth Betta'
+// ตั้งค่า view engine เป็น EJS
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views')); // ตั้งค่า path ของโฟลเดอร์ที่เก็บไฟล์ EJS
 
-const port = process.env.PORT || 5000;
-
-// ใช้ไฟล์สไตล์ และรูปภาพจากโฟลเดอร์ public
+// ใช้ไฟล์สไตล์และรูปภาพจากโฟลเดอร์ public
 app.use(express.static(path.join(__dirname, 'public')));
 
-// ตั้งค่า express-session
-// app.use(session({
- //   secret: '15915963Za', // เปลี่ยนเป็น secret ที่คุณต้องการ
-   // resave: false,
-   // saveUninitialized: true,
-   // cookie: { secure: true } // ใช้ true ถ้าคุณใช้ HTTPS
-// }));
 
-// Middleware เพื่อจัดการการส่งข้อมูลจากฟอร์ม
-app.use(express.urlencoded({ extended: true })); // ใช้สำหรับการรับข้อมูลจากฟอร์ม
+
+// Route สำหรับหน้าแสดงผลผลิตภัณฑ์ต่าง ๆ
+const products = [
+    {
+      route: 'Half_Moon-COPPER',
+      type: 'ปลากัดหางยาว HALF MOON',
+      bettaName: 'Half Moon Copper',
+      mainImage: '/images/blue-betta.jpg',
+      thumbnails: [
+        '/images/blue-betta.jpg',
+        '/images/images-detail/blue-betta-2.jpg',
+        '/images/images-detail/blue-betta-3.jpg'
+      ],
+      PKC: 'HM : 01',
+      price: '120'
+    },
+    {
+      route: 'Half_Moon-SUPER-BLACK',
+      type: 'ปลากัดหางยาว HALF MOON',
+      bettaName: 'Half Moon Super Black',
+      mainImage: '/images/black-betta.jpg',
+      thumbnails: [
+        '/images/black-betta.jpg',
+        '/images/images-detail/black-betta-2.jpg',
+        '/images/images-detail/black-betta-3.jpg'
+      ],
+      PKC: 'HM : 02',
+      price: '180'
+    },
+    {
+      route: 'Half_Moon-PK-COPPER',
+      type: 'ปลากัดหางสั้น',
+      bettaName: 'HALFMOON PLAKAD COPPER',
+      mainImage: '/images/copper-betta.jpg',
+      thumbnails: [
+        '/images/copper-betta.jpg',
+        '/images/images-detail/copper-betta-2.jpg',
+        '/images/images-detail/copper-betta-3.jpg'
+      ],
+      PKC: 'HMPK : 01',
+      price: '150'
+    },
+    {
+      route: 'Half_Moon-PK-NEMO-GALAXY',
+      type: 'ปลากัดหางสั้น',
+      bettaName: 'HALFMOON PLAKAD NEMO GALAXY',
+      mainImage: '/images/galaxy-betta.jpg',
+      thumbnails: [
+        '/images/galaxy-betta.jpg',
+        '/images/images-detail/galaxy-betta-2.jpg',
+        '/images/images-detail/galaxy-betta-3.jpg'
+      ],
+      PKC: 'HMPK : 02',
+      price: '200'
+    },
+    {
+      route: 'Half_Moon-PK-BLACK-DRAGON',
+      type: 'ปลากัดหางสั้น',
+      bettaName: 'HALFMOON PLAKAD BLACK DRAGON',
+      mainImage: '/images/HMPK BLACK DRAGON.jpg',
+      thumbnails: [
+        '/images/HMPK BLACK DRAGON.jpg',
+        '/images/images-detail/HMPK BLACK DRAGON-2.jpg',
+        '/images/images-detail/HMPK BLACK DRAGON-3.jpg'
+      ],
+      PKC: 'HMPK : 02',
+      price: '200'
+    },
+    {
+      route: 'Half_Moon-PK-DUMBO-RED',
+      type: 'ปลากัดหูช้าง',
+      bettaName: 'HALFMOON PLAKAD DUMBO RED',
+      mainImage: '/images/HMPK DUMBO.jpg',
+      thumbnails: [
+        '/images/HMPK DUMBO.jpg',
+        '/images/images-detail/HMPK DUMBO-2.jpg',
+        '/images/images-detail/HMPK DUMBO-3.jpg'
+      ],
+      PKC: 'HMPD : 01',
+      price: '400'
+    },
+    {
+      route: 'Half_Moon-PK-DUMBO-BLUE',
+      type: 'ปลากัดหูช้าง',
+      bettaName: 'HALFMOON PLAKAD DUMBO BLUE',
+      mainImage: '/images/HMPK DUMBO BLUE.jpg',
+      thumbnails: [
+        '/images/HMPK DUMBO BLUE.jpg',
+        '/images/images-detail/HMPK DUMBO BLUE-2.jpg',
+        '/images/images-detail/HMPK DUMBO BLUE-2.jpg'
+      ],
+      PKC: 'HMPD : 02',
+      price: '400'
+    },
+    {
+      route: 'Half_Moon-PK-CROWN-TAIL-FANCY',
+      type: 'ปลากัดหางหนาม',
+      bettaName: 'CROWN TAIL FANCY',
+      mainImage: '/images/CT FANCY.jpg',
+      thumbnails: [
+        '/images/CT FANCY.jpg',
+        '/images/images-detail/CT FANCY-2.jpg',
+        '/images/images-detail/CT FANCY-2.jpg'
+      ],
+      PKC: 'HMCT : 01',
+      price: '500'
+    },
+    // เพิ่มสินค้าอื่น ๆ ในลักษณะเดียวกัน
+  ];
+
+products.forEach(product => {
+  app.get(`/${product.route}`, (req, res) => {
+    res.render('product', { product });
+  });
+});
 
 // กำหนด route สำหรับหน้าเว็บหลัก
 app.get('/', (req, res) => {
-    res.render('index', { title: 'Earth Betta', fish: 'Half Moon COPPER' , Myname: 'Earth'});
+  res.render('index', { req: req, title, products});
 });
 
-app.get('/Half_Moon-COPPER', (req, res) => {
-    res.render('product',{
-        type: 'ปลากัดหางยาว HALF MOON',
-        bettaName: 'Half Moon Copper',
-        mainImage: '/images/blue-betta.jpg',
-        Thumbnail1: '/images/blue-betta.jpg',
-        Thumbnail2: '/images/images-detail/blue-betta-2.jpg',
-        Thumbnail3: '/images/images-detail/blue-betta-3.jpg',
-        PKC: 'HM : 01',
-        price: '120'
-    });
-});
-
-app.get('/Half_Moon-SUPER-BLACK', (req, res) => {
-    res.render('product',{
-        type: 'ปลากัดหางยาว HALF MOON',
-        bettaName: 'Half Moon Super Black',
-        mainImage: '/images/black-betta.jpg',
-        Thumbnail1: '/images/black-betta.jpg',
-        Thumbnail2: '/images/images-detail/black-betta-2.jpg',
-        Thumbnail3: '/images/images-detail/black-betta-3.jpg',
-        PKC: 'HM : 02',
-        price: '180'
-    });
-});
-
-app.get('/Half_Moon-PK-COPPER', (req, res) => {
-    res.render('product',{
-        type: 'ปลากัดหางสั้น',
-        bettaName: 'HALFMOON PLAKAD COPPER',
-        mainImage: '/images/copper-betta.jpg',
-        Thumbnail1: '/images/copper-betta.jpg',
-        Thumbnail2: '/images/images-detail/copper-betta-2.jpg',
-        Thumbnail3: '/images/images-detail/copper-betta-3.jpg',
-        PKC: 'HMPK : 01',
-        price: '150'
-    });
-});
-
-app.get('/Half_Moon-PK-NEMO-GALAXY', (req, res) => {
-    res.render('product',{
-        type: 'ปลากัดหางสั้น',
-        bettaName: 'HALFMOON PLAKAD NEMO GALAXY',
-        mainImage: '/images/galaxy-betta.jpg',
-        Thumbnail1: '/images/galaxy-betta.jpg',
-        Thumbnail2: '/images/images-detail/galaxy-betta-2.jpg',
-        Thumbnail3: '/images/images-detail/galaxy-betta-3.jpg',
-        PKC: 'HMPK : 02',
-        price: '200'
-    });
-});
-
-app.get('/Half_Moon-PK-BLACK-DRAGON', (req, res) => {
-    res.render('product',{
-        type: 'ปลากัดหางสั้น',
-        bettaName: 'HALFMOON PLAKAD BLACK DRAGON',
-        mainImage: '/images/HMPK BLACK DRAGON.jpg',
-        Thumbnail1: '/images/HMPK BLACK DRAGON.jpg',
-        Thumbnail2: '/images/images-detail/HMPK BLACK DRAGON-2.jpg',
-        Thumbnail3: '/images/images-detail/HMPK BLACK DRAGON-3.jpg',
-        PKC: 'HMPK : 03',
-        price: '300'
-    });
-});
-
-app.get('/Half_Moon-PK-DUMBO-RED', (req, res) => {
-    res.render('product',{
-        type: 'ปลากัดหูช้าง',
-        bettaName: 'HALFMOON PLAKAD DUMBO RED',
-        mainImage: '/images/HMPK DUMBO.jpg',
-        Thumbnail1: '/images/HMPK DUMBO.jpg',
-        Thumbnail2: '/images/images-detail/HMPK DUMBO-2.jpg',
-        Thumbnail3: '/images/images-detail/HMPK DUMBO-3.jpg',
-        PKC: 'HMD : 01',
-        price: '400'
-    });
-});
-
-app.get('/Half_Moon-PK-DUMBO-BLUE', (req, res) => {
-    res.render('product',{
-        type: 'ปลากัดหูช้าง',
-        bettaName: 'HALFMOON PLAKAD DUMBO BLUE',
-        mainImage: '/images/HMPK DUMBO BLUE.jpg',
-        Thumbnail1: '/images/HMPK DUMBO BLUE.jpg',
-        Thumbnail2: '/images/images-detail/HMPK DUMBO BLUE-2.jpg',
-        Thumbnail3: '/images/images-detail/HMPK DUMBO BLUE-3.jpg',
-        PKC: 'HMD : 02',
-        price: '400'
-    });
-});
-
-app.get('/Half_Moon-PK-CROWN-TAIL-FANCY', (req, res) => {
-    res.render('product',{
-        type: 'ปลากัดหางหนาม',
-        bettaName: 'CROWN TAIL FANCY',
-        mainImage: '/images/CT FANCY.jpg',
-        Thumbnail1: '/images/CT FANCY.jpg',
-        Thumbnail2: '/images/images-detail/CT FANCY-2.jpg',
-        Thumbnail3: '/images/images-detail/CT FANCY-3.jpg',
-        PKC: 'HMCT : 01',
-        price: '500'
-    });
-});
-
-// Route สำหรับเพิ่มสินค้าลงในตะกร้า
-app.post('/add-to-cart', (req, res) => {
-    const { bettaName, price, quantity } = req.body;
-
-    if (!req.session.cart) {
-        req.session.cart = []; // สร้างตะกร้าใหม่หากยังไม่มี
-    }
-
-    // เพิ่มสินค้าลงในตะกร้า
-    req.session.cart.push({ bettaName, price, quantity });
-    
-    // เช็คปุ่มที่กด
-    if (req.body.action === 'buy') {
-        return res.redirect('/cart'); // เปลี่ยนไปที่หน้าตะกร้า
-    }
-    
-    // กลับไปที่หน้าผลิตภัณฑ์หลังจากหยิบลงตะกร้า
-    res.redirect('back');
-});
-
-
-// Route สำหรับหน้าแสดงผลตะกร้า
 app.get('/cart', (req, res) => {
-    const cartItems = req.session.cart || []; // ดึงข้อมูลตะกร้าสินค้า
-    res.render('cart', { cartItems }); // ส่งข้อมูลไปยัง template
+  const name = req.query.name;
+  const price = req.query.price;
+  const img = req.query.img;
+
+  // ตรวจสอบถ้ามีข้อมูลสินค้า
+  const hasProduct = name && price && img;
+
+  res.render('cart', { name, price, img, hasProduct });
 });
 
+
+app.get('/payment', (req, res) => {
+  res.render('payment');
+});
+
+// เริ่มเซิร์ฟเวอร์
 app.listen(port, () => {
     console.log(`Server listening on port ${port}`);
-  });
+});
